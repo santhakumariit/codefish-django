00c1a0807128b221b9ad51e977f09c44a4b4c804
Clemens Helm <clemens.helm@gmail.com>
2014-01-02 12:18:21 +0100

    Deactivated message middleware.

--- codefish/settings.py
+++ codefish/settings.py
@@ -31,7 +31,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
 #    'django.middleware.csrf.CsrfViewMiddleware',
 #    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+#    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
